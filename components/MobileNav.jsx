2ff4969d0a8797866ebbe7f1e93b28704dd14287
Atharva Mali <atharva.mali3081@gmail.com>
2024-07-02 14:56:18 +0530

    MobileNav auto close fix

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Link from "next/link";
@@ -29,28 +30,56 @@ const links = [
 ];
 
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
-  return <Sheet>
-    <SheetTrigger className="flex justify-center items-center">
-      <CiMenuFries className="text-[32px] text-accent" />
-    </SheetTrigger>
-    <SheetContent className="flex flex-col">
-      {/* logo */}
-      <div className="mt-32 mb-40 text-center text-2xl">
-        <Link href="/">
-        <h1>Atharva<span className="text-accent">.</span></h1>
-        </Link>
-      </div>
-      {/* nav */}
-      <nav className="flex flex-col justify-center items-center gap-8">
-        {links.map((link, index)=>{
-          return <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
-            {link.name}
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const handleLinkClick = (path) => () => {
+    setIsOpen(false);
+    // Use a short delay to ensure the link navigation is not interrupted
+    setTimeout(() => {
+      document.location.href = path;
+    }, 100);
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(true)}
+      >
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" onClick={handleLinkClick("/")}>
+            <h1>
+              Atharva<span className="text-accent">.</span>
+            </h1>
           </Link>
-        })}
-      </nav>
-    </SheetContent>
-  </Sheet>;
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              className={`${
+                link.path === pathname && "text-accent border-b-2 border-accent"
+              } text-xl capitalize hover:text-accent transition-all`}
+              onClick={handleLinkClick(link.path)}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
